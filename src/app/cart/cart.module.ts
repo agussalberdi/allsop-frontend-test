570c821042b379270807da408be7dfb628e78406
Agustin Alberdi <agustinalberdi1@gmail.com>
2020-04-03 12:30:25 +0200

    Cart module: It was unnecessary to have a cart component without logic. Cart-table renamed to cart and old cart deleted.

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -5,12 +5,10 @@ import { CartRoutingModule } from './cart-routing.module';
 import { SharedModule } from './../shared/shared.module';
 
 import { CartComponent } from './containers/cart/cart.component';
-import { CartTableComponent } from './components/cart-table/cart-table.component';
 
 @NgModule({
   declarations: [
-    CartComponent,
-    CartTableComponent
+    CartComponent
   ],
   imports: [
     CommonModule,
--- src/app/cart/containers/cart/cart.component.ts
+++ src/app/cart/containers/cart/cart.component.ts
@@ -1,12 +1,100 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Product } from 'src/app/shared/interfaces/product.interface';
+import { Category } from 'src/app/shared/enums/category.enum';
+import { CartService } from '../../services/cart.service';
+import { ProductsService } from './../../../products/services/products.service';
 
 @Component({
     selector: 'app-cart',
     templateUrl: './cart.component.html',
     styleUrls: ['./cart.component.scss']
 })
-export class CartComponent implements OnInit {
-    constructor() {}
+export class CartComponent implements OnInit, OnDestroy {
+    displayedColumns: string[] = ['item', 'cost', 'remove'];
+    cart: Product[];
+    subscription: Subscription;
 
-    ngOnInit() {}
+    total: number;
+
+    discountDrinks = false;
+    discountIngredients = false;
+    discountVoucher = false;
+    discountDrinksMessage: string;
+    discountIngredientsMessage: string;
+    discountVoucherMessage: string;
+
+    constructor(private cartService: CartService, private productsService: ProductsService) {}
+
+    ngOnInit() {
+        this.subscription = this.cartService.cart$.subscribe(cart => this.cart = cart);
+        this.getTotalAfterDiscounts();
+    }
+
+    removeProduct(product: Product, i: number) {
+        this.cartService.removeCart(i);
+        this.productsService.increaseQuantity(product.name);
+        this.getTotalAfterDiscounts();
+    }
+
+    getCartTotal() {
+        return this.cartService.getTotal();
+    }
+
+    applyVoucher(voucher: Event) {
+        if (voucher && this.total >= 100) {
+            this.total -= 20;
+            this.discountVoucher = true;
+            this.discountVoucherMessage = 'Promotional Code: 20£ off';
+        } else {
+            this.discountVoucher = false;
+        }
+    }
+
+    getDiscountDrinks() {
+        const drinks = this.cart.filter(product => product.category === Category.Drinks);
+
+        if (drinks.length >= 10) {
+            return drinks.reduce((total = 0, product: Product) => {
+                this.discountDrinks = true;
+                this.discountDrinksMessage = 'Drinks 10% off';
+                if (product.reduced) {
+                    return total + (product.reduced * 0.1);
+                }
+                return total + (product.price * 0.1);
+            }, 0);
+        }
+        this.discountDrinks = false;
+        return 0;
+    }
+
+    getCookingAndBakingDiscounts() {
+        const ingredientsCheck = this.cart.reduce((total = 0, product: Product) => {
+            if (product && product.category === Category.Ingredients) {
+                if (product.reduced) {
+                    return total + product.reduced;
+                }
+                return total + product.price;
+            }
+        }, 0);
+
+        if (ingredientsCheck >= 50) {
+            this.discountIngredients = true;
+            this.discountIngredientsMessage = 'Baking/Cooking Ingredients 5£ off';
+            return 5;
+        }
+        this.discountIngredients = false;
+        return 0;
+    }
+
+    getTotalAfterDiscounts() {
+        const cartTotal = this.getCartTotal();
+        const drinksOff = this.getDiscountDrinks();
+        const ingredientsOff = this.getCookingAndBakingDiscounts();
+        this.total = cartTotal - drinksOff - ingredientsOff;
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 }
